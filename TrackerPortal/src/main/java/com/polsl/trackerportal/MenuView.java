02d6c75980e008eea442d3b77d01756c24576284
mateusz1994a@gmail.com <zgadnij123>
2017-09-26 13:36:57 +0200

    portal

---
+++ TrackerPortal/src/main/java/com/polsl/trackerportal/MenuView.java
@@ -0,0 +1,86 @@
+/*
+(c) Systemy Przetwarzania i Integracji Danych SPIID sp. z o.o.
+1:1 Realny obraz Twojej firmy
+ */
+package com.polsl.trackerportal;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
+import org.primefaces.model.menu.DefaultMenuItem;
+/**
+ *
+ * @author Mateusz Ligus <mateusz.ligus@spiid.pl>
+ */
+@ManagedBean(name = "menuView")
+@SessionScoped
+public class MenuView implements Serializable {
+
+    private MenuModel model;
+
+    public MenuModel getModel() {
+        return model;
+    }
+
+    public void setModel(MenuModel model) {
+        this.model = model;
+    }
+    private String name;
+
+    public MenuView() {
+        name = "Alalalala";
+    }
+
+    @PostConstruct
+    public void init() {
+        createMenuModel();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private void createMenuModel() {
+        model = new DefaultMenuModel();
+
+//First submenu
+        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Dynamic Submenu");
+
+        DefaultMenuItem item = new DefaultMenuItem("External");
+        item.setUrl("http://www.primefaces.org");
+        item.setIcon("ui-icon-home");
+        firstSubmenu.addElement(item);
+
+        model.addElement(firstSubmenu);
+
+//Second submenu
+        DefaultSubMenu secondSubmenu = new DefaultSubMenu("Dynamic Actions");
+
+        item = new DefaultMenuItem("Save");
+        item.setIcon("ui-icon-disk");
+        item.setCommand("#{menuBean.save}");
+        secondSubmenu.addElement(item);
+
+        item = new DefaultMenuItem("Delete");
+        item.setIcon("ui-icon-close");
+        item.setCommand("#{menuBean.delete}");
+        item.setAjax(false);
+        secondSubmenu.addElement(item);
+
+        item = new DefaultMenuItem("Redirect");
+        item.setIcon("ui-icon-search");
+        item.setCommand("#{menuBean.redirect}");
+        secondSubmenu.addElement(item);
+
+        model.addElement(secondSubmenu);
+    }
+
+}
