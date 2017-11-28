ca0fdda4f8c482ac21efd07ecabf5047dc87adc3
matelig <mateusz1994a@gmail.com>
2017-11-28 23:09:42 +0100

    english text fixes

--- TrackerPortal/target/generated-sources/annotations/com/polsl/trackerportal/database/entity/TroubleCodesNames_.java
+++ TrackerPortal/target/generated-sources/annotations/com/polsl/trackerportal/database/entity/TroubleCodesNames_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-28T22:47:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-28T23:08:34")
 @StaticMetamodel(TroubleCodesNames.class)
 public class TroubleCodesNames_ { 
 
